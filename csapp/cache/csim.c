5eea85256bbe4d7e270b2f8a4f6616bad178bc5b
ste11a <1458334925@qq.com>
2021-05-04 17:32:18 +0800

    cachelab的csim

--- csapp/cache/csim.c
+++ csapp/cache/csim.c
@@ -1,7 +1,166 @@
+/* simulate the cache mem */
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <math.h>
+#include <string.h>
 
-int main()
+// each line in the cache
+typedef struct
 {
-    printSummary(0, 0, 0);
+    int valid;
+    unsigned long tag;
+    int times; // cnt for not use time, used for LRU
+} Line;
+
+void parse(char *trace);
+void visit(unsigned long set, unsigned long tag);
+
+int verbose = 0;                  // whether verbose mode
+int set_bit, block_bit, tag_bit;  // addr's diff part's bit size
+int line_size;                    // line size of cache's set
+unsigned long set_mask, tag_mask; // used to get set and tag from addr
+
+// count the total hit, miss, evict of access
+int hit_cnt = 0, miss_cnt = 0, eviction_cnt = 0;
+Line *cache; // the simulated cache
+
+int main(int argc, char *argv[])
+{
+    char *file_name;
+    // parses the command-line arguments
+    int opt;
+    while ((opt = getopt(argc, argv, "vs:E:b:t:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'v':
+            verbose = 1;
+            break;
+        case 's':
+            set_bit = atoi(optarg);
+            break;
+        case 'E':
+            line_size = atoi(optarg);
+            break;
+        case 'b':
+            block_bit = atoi(optarg);
+            break;
+        case 't':
+            file_name = optarg;
+            break;
+        default: /* '?' */
+            fprintf(stderr, "Usage: %s [-v] -s <s> -E <E> -b <b> -t <tracefile>\n",
+                    argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    tag_bit = 64 - set_bit - block_bit;
+    // compute mask
+    tag_mask = ((1 << tag_bit) - 1) << (64 - tag_bit);
+    set_mask = ((1 << set_bit) - 1) << block_bit;
+
+    // allocate space for the cache
+    cache = (Line *)malloc(sizeof(Line) * (1 << set_bit) * line_size);
+    // open the file and parse each trace
+    FILE *fp = fopen(file_name, "r");
+    char trace[1024] = {0}; // store each trace
+    while (fgets(trace, sizeof(trace), fp) != NULL)
+    {
+        parse(trace);
+    }
+    fclose(fp);
+    free(cache);
+    printSummary(hit_cnt, miss_cnt, eviction_cnt);
     return 0;
+}
+/**
+ * parse the trace from the file
+ * @para: the string prased
+ * 
+*/
+void parse(char *trace)
+{
+    // ignore instruction cache access
+    if (trace[0] == 'I')
+        return;
+    // get addr from the trace
+    // ignore request size (beacuse mem access not cross block boundaries)
+    unsigned long addr;
+    int tmp;
+    sscanf(trace + 3, "%lx,%d", &addr, &tmp);
+    // get the position in cache
+    unsigned long set = (addr & set_mask) >> (block_bit);
+    unsigned long tag = (addr & tag_mask) >> (64 - tag_bit);
+    switch (trace[1])
+    {
+    case 'L': // load data
+    case 'S': // store data
+        visit(set, tag);
+        break;
+    case 'M': // modify data
+        visit(set, tag);
+        visit(set, tag);
+        break;
+    }
+}
+
+/**
+ * visit the cache's block 
+ * @para: set - the set block maped to 
+ *        tag - tag's content stored in line
+*/
+void visit(unsigned long set, unsigned long tag)
+{
+    int base = set * line_size; // set's first line
+    int free_index = -1, hit_pos = -1;
+    int victim = base;
+    // traverse every line in the set
+    for (int i = base; i < base + line_size; i++)
+    {
+
+        if (cache[i].valid) // valid
+        {
+            if (cache[i].tag == tag) // hit
+            {
+                hit_pos = i;
+                break;
+            }
+            else if (free_index < 0 && // the block least read
+                     cache[i].times > cache[victim].times)
+            {
+                victim = i;
+            }
+        }
+        else if (free_index < 0) // 1st free block
+        {
+            free_index = i;
+        }
+    }
+    if (hit_pos != -1) // hit
+    {
+        hit_cnt++;
+        cache[hit_pos].times = -1;
+    }
+    else // miss
+    {
+        miss_cnt++;
+        if (free_index < 0) // conflict
+        {
+            eviction_cnt++;
+            free_index = victim;
+        }
+        // cache the new block
+        cache[free_index].valid = 1;
+        cache[free_index].tag = tag;
+        cache[free_index].times = -1;
+    }
+
+    // update times
+    for (int i = base; i < base + line_size; i++)
+    {
+        cache[i].times++;
+    }
 }
--- csapp/cache/trans.c
+++ csapp/cache/trans.c
@@ -1,3 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-29 22:40:59
+ * @LastEditTime: 2021-04-29 22:41:00
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /cache/trans.c
+ */
+
 /* 
  * trans.c - Matrix transpose B = A^T
  *
@@ -6,7 +15,7 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
@@ -27,7 +36,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
- */ 
+ */
 
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
@@ -37,13 +46,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -56,11 +66,10 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(trans, trans_desc);
 }
 
 /* 
@@ -72,13 +81,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
